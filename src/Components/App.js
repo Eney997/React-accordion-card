f32c7ed7410ae76e8f4a108b4db7f87459023496
tornike dzidziguri <tornikedzidziguri2001@gmail.com>
2024-01-13 00:25:12 +0400

    added/map

---
+++ src/Components/App.js
@@ -0,0 +1,27 @@
+export const info = [
+    {
+        id:1,
+        question: 'How many team members can I invite?',
+        answer:'The number of team members is not limited.',
+    },
+    {
+        id:2,
+        question: 'What is the maximum file upload size?',
+        answer:'No more than 2GB. All files in your account must fit your allotted storage space.',
+    },
+    {
+        id:3,
+        question: 'How do I reset my password?',
+        answer:'We will send you a link to reset your password on Gmail.',
+    },
+    {
+        id:4,
+        question: 'Can I cancel my subscription?',
+        answer:'Of course, you can cancel after one month for free.',
+    },
+    {
+        id:5,
+        question: 'Do you provide additional support?',
+        answer:'You can find all kinds of information on the website.',
+    },
+]
--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -4,49 +4,26 @@ import './Content.css'
 import mobImg from '../public/mobFullPick.svg'
 import arrowMob from '../public/arrowMob.svg'
 import shadowMob from '../public/shadowMob.svg'
+import { info } from './App'
 
-const Content = () => {
-    const [selected,setSelected] = useState(null)
-    const toggle = (i) => {
-        if (selected = i ) {
-         return setSelected(null)
-        }
-        setSelected(i)
-    } 
+const Content = ({answer,question}) => {
+  
   return (
     <Card>
         <img src={mobImg} className='mobImge'></img>
         <img src={shadowMob} className='shadowMob'></img>
         <h1 className='Faq'>FAQ</h1>
         <div className='accordion'>
-            <h1 className='forH'>How many team members can I invite?{selected}</h1>
-            <img src={arrowMob} className='arrowImg'></img>
-            <p className='forP'>The number of team members is not limited.</p>
-            <div className='underLine'></div>
-
-
-            <h2 className='forH'>What is the maximum file upload size?</h2>
-            <img src={arrowMob} className='arrowImg'></img>
-            <p className='forP'>No more than 2GB. All files in your account must fit your allotted storage space.</p>
-            <div className='underLine'></div>
-
-
-            <h3 className='forH'>How do I reset my password?</h3>
-            <img src={arrowMob} className='arrowImg'></img>
-            <p className='forP'>We will send you a link to reset your password on Gmail.</p>
-            <div className='underLine'></div>
-
-
-            <h4 className='forH'>Can I cancel my subscription?</h4>
-            <img src={arrowMob} className='arrowImg'></img>
-            <p className='forP'>Of course, you can cancel after one month for free.</p>
-            <div className='underLine'></div>
-
-
-            <h5 className='forH'>Do you provide additional support?</h5>
-            <img src={arrowMob} className='arrowImg'></img>
-            <p className='forP'>You can find all kinds of information on the website.</p>
-            <div className='underLine'></div>
+            {info.map((item,i)=>(
+            <div key={i}>
+               <div className={question}>
+                <h2 id='tittle'>{item.question}</h2>
+               </div>
+               <img src={arrowMob} className='arrowImg'></img>
+               <div id='content' className={answer}>{item.answer}</div>
+               <div className='underLine'></div>
+            </div>
+            ))}
         </div>
     </Card>
   )
